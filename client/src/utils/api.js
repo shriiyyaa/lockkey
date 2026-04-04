3eaec5a1334e7ce64cc6eed2c5d37859bb6f4514
Shriya Nayyar <nayyarshriya825@gmail.com>
2026-04-05 00:43:20 +0530

    Reduce API timeout to 5 seconds per user request

--- client/src/utils/api.js
+++ client/src/utils/api.js
@@ -2,7 +2,7 @@ import axios from 'axios';
 
 const api = axios.create({
   baseURL: 'https://lockkey-h8ib.onrender.com/api',
-  timeout: 15000,
+  timeout: 5000,
   headers: {
     'Content-Type': 'application/json'
   }
